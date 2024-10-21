d0fefbfd78ae16b541cada408eb6f821c1462d7e
Artem Makeev <Kochebnic@gmail.com>
2024-10-21 07:47:39 +0300

    2

--- src/Creator/Creator.ts
+++ src/Creator/Creator.ts
@@ -1,5 +1,7 @@
 import { CreatorInterface } from "./Creator.Interface";
 import { InitStart } from "./CreatorFn/CreatorFnList/InitStart";
+import { InitScenario } from "./CreatorFn/CreatorFnList/InitScenario";
+import { InitCommand } from "./CreatorFn/CreatorFnList/InitCommand";
 
 export class Creator implements CreatorInterface.ICreator {
 	commandInit: CreatorInterface.IBase;
@@ -7,8 +9,8 @@ export class Creator implements CreatorInterface.ICreator {
 	startInit: CreatorInterface.IBase;
 
 	constructor(module: CreatorInterface.IClass, plugin: CreatorInterface.IPlugin) {
-		this.commandInit = new InitStart(module, plugin);
-		this.scenarioInit = new InitStart(module, plugin);
+		this.commandInit = new InitCommand(module, plugin);
+		this.scenarioInit = new InitScenario(module, plugin);
 		this.startInit = new InitStart(module, plugin);
 	}
 
